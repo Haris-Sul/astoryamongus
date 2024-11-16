6a4956eae5db74862b9904ec2059e3b879462f70
Marco Manivannan <mmanivannan1@sheffield.ac.uk>
2024-11-16 22:00:35 +0000

    feat: implemented Flask-SocketIO rooms.
    Need to create the HTML for the room.

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, session
-from flask_socketio import SocketIO
+from flask_socketio import join_room, leave_room, send, SocketIO
 import secrets
 import string
 
@@ -21,16 +21,16 @@ def generate_unique_room_id(length):
 def index():
     session.clear()
     if request.method == "POST":
-        room_name = request.form.get("room_name")
+        user_name = request.form.get("user_name")
         create = request.form.get("create", False)
         room_id = request.form.get("room_id")
         join = request.form.get("join", False)
         
-        if not room_name:
-            return render_template("index.html", error="Please enter a room name.", room_id=room_id, room_name=room_name)
+        if not user_name:
+            return render_template("index.html", error="Please enter a user name.", room_id=room_id, user_name=user_name)
         
         if join != False and not room_id:
-            return render_template("index.html", error="Please enter a room ID.", room_id=room_id, room_name=room_name)
+            return render_template("index.html", error="Please enter a room ID.", room_id=room_id, user_name=user_name)
         
         room = room_id
         # If you are creating a room (create button is pressed)
@@ -39,10 +39,10 @@ def index():
             rooms[room] = {"members": 0, "messages": []}
         # Otherwise, you are joining a room. If room does not exist with that ID.
         elif room_id not in rooms:
-            return render_template("index.html", error="Room does not exist.", room_id=room_id, room_name=room_name)
+            return render_template("index.html", error="Room does not exist.", room_id=room_id, user_name=user_name)
             
         session["room"] = room
-        session["room_name"] = room_name
+        session["user_name"] = user_name
         
         return redirect(url_for("room"))
         
@@ -51,9 +51,25 @@ def index():
 @app.route("/room")
 def room():
     room = session.get("room")
-    if room is None or session.get("name") is None or room not in rooms:
+    if room is None or session.get("user_name") is None or room not in rooms:
         return redirect(url_for("index"))
     return render_template("room.html")
+
+@socketio.on("connect")
+def connect(auth):
+    room = session.get("room")
+    user_name = session.get("user_name")
+    
+    if not room or not user_name:
+        return
+    if room not in rooms:
+        leave_room(room)
+        return
+    
+    join_room(room)
+    send({"name": user_name, "message": " has entered the room."}, to=room)
+    rooms[room]["members"] += 1
+    print(f"{user_name} joined room {room}")
 
 if __name__ == "__main__":
     socketio.run(app, debug=True)
